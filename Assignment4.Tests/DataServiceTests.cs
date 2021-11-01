4bfdca3f4532be2ccca23ef84dd8f8ba16cab7b3
serubi <serubi@live.com>
2021-11-01 13:34:17 +0100

    Potential special character fix on MacOS

--- Assignment4.Tests/DataServiceTests.cs
+++ Assignment4.Tests/DataServiceTests.cs
@@ -124,7 +124,7 @@ namespace Assignment4.Tests
             Assert.Equal(12, products.Count);
             Assert.Equal("Chai", products.First().Name);
             Assert.Equal("Beverages", products.First().CategoryName);
-            Assert.Equal("Lakkalikˆˆri", products.Last().Name);
+            Assert.Equal("Lakkalik√∂√∂ri", products.Last().Name);
         }
 
         [Fact]
@@ -133,7 +133,7 @@ namespace Assignment4.Tests
             var service = new DataService();
             var products = service.GetProductByName("em");
             Assert.Equal(4, products.Count);
-            Assert.Equal("NuNuCa Nuﬂ-Nougat-Creme", products.First().ProductName);
+            Assert.Equal("NuNuCa Nu√ü-Nougat-Creme", products.First().ProductName);
             Assert.Equal("Flotemysost", products.Last().ProductName);
         }
 
